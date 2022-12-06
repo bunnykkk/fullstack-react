29ff9a21c894afc8e8963eb9384e76d7e90e8066
Adilet <baryktabasovadi@gmail.com>
2022-12-06 18:50:05 +0600

    Buy edited

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,6 +10,9 @@ import Paper from "@mui/material/Paper";
 import { useCart } from "../../contexts/CartContextProvider";
 import { Button, TextField, Typography } from "@mui/material";
 import { useProducts } from "../../contexts/ProductContextProvider";
+import "../../styles/Cart.css";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 export default function Cart() {
   const { getCart, cart, changeProductCount, deleteProductInCart } = useCart();
@@ -29,54 +32,202 @@ export default function Cart() {
     getCart();
   }
 
+  let styleText = {
+    color: "white",
+  };
+
+  // modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Picture</TableCell>
-            <TableCell align="center">Name</TableCell>
-            <TableCell align="center">Type</TableCell>
-            <TableCell align="center">Price</TableCell>
-            <TableCell align="center">Count</TableCell>
-            <TableCell align="center">Sub Price</TableCell>
-            <TableCell align="center">---</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cart?.products.map(row => (
-            <TableRow
-              key={row.item.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <TableCell>
-                <img src={row.item.picture} alt="" width="50" />
+    <>
+      <TableContainer className="cartBlock">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow className="textBlockCart">
+              <TableCell style={styleText}>Picture</TableCell>
+              <TableCell style={styleText} align="center">
+                Name
               </TableCell>
-              <TableCell align="center">{row.item.name}</TableCell>
-              <TableCell align="center">{row.item.type}</TableCell>
-              <TableCell align="center">{row.item.price}</TableCell>
-              <TableCell align="center">
-                <TextField
-                  type="number"
-                  value={row.count}
-                  onChange={e =>
-                    changeProductCount(e.target.value, row.item.id)
-                  }
-                />
+              <TableCell style={styleText} align="center">
+                Type
               </TableCell>
-              <TableCell align="center">{row.subPrice}</TableCell>
-              <TableCell align="center">
-                <button onClick={() => deleteProductInCart(row.item.id)}>
-                  Delete From Cart
-                </button>
+              <TableCell style={styleText} align="center">
+                Price
+              </TableCell>
+              <TableCell style={styleText} align="center">
+                Count
+              </TableCell>
+              <TableCell style={styleText} align="center">
+                Sub Price
+              </TableCell>
+              <TableCell style={styleText} align="center">
+                ---
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Typography variant="h6" component="div">
-        Total Price: {cart?.totalPrice}
-        <Button onClick={cartCleaner}>BUY NOW</Button>
-      </Typography>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {cart?.products.map((row) => (
+              <TableRow
+                key={row.item.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell>
+                  <img src={row.item.picture} alt="" width="50" />
+                </TableCell>
+                <TableCell align="center">{row.item.name}</TableCell>
+                <TableCell align="center">{row.item.type}</TableCell>
+                <TableCell align="center">{row.item.price}</TableCell>
+                <TableCell align="center">
+                  <TextField
+                    type="number"
+                    value={row.count}
+                    onChange={(e) =>
+                      changeProductCount(e.target.value, row.item.id)
+                    }
+                  />
+                </TableCell>
+                <TableCell align="center">{row.subPrice}</TableCell>
+                <TableCell align="center">
+                  <button onClick={() => deleteProductInCart(row.item.id)}>
+                    Delete From Cart
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Typography
+          variant="h6"
+          component="div"
+          style={{
+            marginTop: "1vmax",
+            marginBottom: "1vmax",
+          }}>
+          Total Price: {cart?.totalPrice}
+          <Button onClick={handleOpen} className="buyButton btn1">
+            BUY NOW
+          </Button>
+        </Typography>
+      </TableContainer>
+      <div style={{ backgroundColor: "white" }}>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <Box className="modalStyle">
+            <h2>Ordering</h2>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: "20rem",
+              }}>
+              <TextField
+                id="standard-search"
+                label="First name*"
+                type="search"
+                variant="standard"
+                style={{ width: "9rem" }}
+              />
+              <TextField
+                id="standard-search"
+                label="Last name*"
+                type="search"
+                variant="standard"
+                style={{ width: "9rem" }}
+              />
+            </div>
+            <div>
+              <TextField
+                id="standard-search"
+                label="Email@adress"
+                type="email"
+                variant="standard"
+                style={{ width: "20rem" }}
+              />
+            </div>
+            <div>
+              <TextField
+                id="standard-search"
+                label="Address street 1"
+                type="search"
+                variant="standard"
+                style={{ width: "20rem" }}
+              />{" "}
+            </div>{" "}
+            <div>
+              <TextField
+                id="standard-search"
+                label="Address street 2"
+                type="address"
+                variant="standard"
+                style={{ width: "20rem" }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: "20rem",
+              }}>
+              <TextField
+                id="standard-search"
+                label="City*"
+                type="search"
+                variant="standard"
+              />
+              <TextField
+                id="standard-search"
+                label="Region"
+                type="search"
+                variant="standard"
+              />
+            </div>
+            <div>
+              <TextField
+                id="standard-search"
+                label="Add card number"
+                type="email"
+                variant="standard"
+                style={{ width: "20rem" }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: "20rem",
+              }}>
+              <TextField
+                id="standard-search"
+                label="CVV"
+                type="search"
+                variant="standard"
+              />
+              <TextField
+                id="standard-search"
+                label="Month Date"
+                type="number"
+                variant="standard"
+              />
+            </div>
+            <button
+              onClick={() => {
+                cartCleaner();
+                handleClose();
+              }}
+              className="buyButton">
+              BUY
+            </button>
+          </Box>
+        </Modal>
+      </div>
+    </>
   );
 }
