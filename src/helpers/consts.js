da741165bcdd162f4ba58b8fc0493b2c3804b794
ramkkk <ramin.charipchynkanlu@mail.ru>
2022-12-08 14:32:11 +0600

    deploy

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -8,4 +8,4 @@ export const CART = {
   GET_CART_LENGTH: "GET_CART_LENGTH",
 };
 
-export const JSON_API_PRODUCTS = "http://34.239.103.140";
+export const JSON_API_PRODUCTS = "http://apparat.vercel.com/products";
